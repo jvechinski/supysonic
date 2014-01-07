82fe2906489ffbee14f47aac62c6d76961112c7c
jvechinski <jvechinski@gmail.com>
2014-01-06 21:21:45 -0600

    Allow use without any configuration file.

--- supysonic/config.py
+++ supysonic/config.py
@@ -2,30 +2,39 @@
 
 import os, sys, tempfile, ConfigParser
 
-config = ConfigParser.RawConfigParser({ 'cache_dir': os.path.join(tempfile.gettempdir(), 'supysonic') })
+default_config_values = {
+    'cache_dir': os.path.join(tempfile.gettempdir(), 'supysonic'),
+    # @todo Will this syntax work on Windows???
+    'database_uri': 'sqlite:///'+ os.path.expanduser('~/.supysonic.db'),
+}   
+
+config_sections_when_no_config_file = ['base'] 
+
+config = ConfigParser.RawConfigParser(default_config_values)
 
 def check():
-	try:
-		ret = config.read([ '/etc/supysonic', os.path.expanduser('~/.supysonic') ])
-	except (ConfigParser.MissingSectionHeaderError, ConfigParser.ParsingError), e:
-		print >>sys.stderr, "Error while parsing the configuration file(s):\n%s" % str(e)
-		return False
+    try:
+        ret = config.read([ '/etc/supysonic', os.path.expanduser('~/.supysonic') ])
+    except (ConfigParser.MissingSectionHeaderError, ConfigParser.ParsingError), e:
+        print >>sys.stderr, "Error while parsing the configuration file(s):\n%s" % str(e)
+        return False
 
-	if not ret:
-		print >>sys.stderr, "No configuration file found"
-		return False
+    if not ret:
+        for section in config_sections_when_no_config_file:
+            config.add_section(section)
+        
+        print("WARNING: No configuration file found, using default values for all configuration parameters")
+        return True
 
-	try:
-		config.get('base', 'database_uri')
-	except:
-		print >>sys.stderr, "No database URI set"
-		return False
+    #if not config.has_option('base', 'database_uri'):
+    #    print("WARNING: No database URI set, using default {}".format(
+    #        default_config_values['database_uri']))     
 
-	return True
+    return True
 
 def get(section, name):
-	try:
-		return config.get(section, name)
-	except:
-		return None
+    try:
+        return config.get(section, name)
+    except:
+        return None
 
